2908d5dcf377ffa1244ab7033647e0a084d845ef
valiclud <valicek@atlas.cz>
2023-05-12 19:16:42 +0200

    Adding patch and delete REST methods to client

--- taco-cloud-jpa/src/main/java/tacos/data/ClientService.java
+++ taco-cloud-jpa/src/main/java/tacos/data/ClientService.java
@@ -7,16 +7,19 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import tacos.dto.ClientDto;
 import tacos.entity.Client;
+import tacos.web.api.TacoClientController;
 
+@Slf4j
 @Service
 public class ClientService {
 
   @Autowired
   ClientRepository clientRepository;
-  
-  public List<ClientDto> findAll(){
+
+  public List<ClientDto> findAll() {
     Iterable<Client> clients = this.clientRepository.findAll();
     List<ClientDto> clientsDto = new ArrayList<>();
     if (clients.iterator().hasNext()) {
@@ -27,8 +30,8 @@ public class ClientService {
     }
     return clientsDto;
   }
-  
-  public List<ClientDto> findByDeliveryName(String deliveryName){
+
+  public List<ClientDto> findByDeliveryName(String deliveryName) {
     List<Client> clients = this.clientRepository.findByDeliveryName(deliveryName);
     List<ClientDto> clientsDto = new ArrayList<>();
     if (!clients.isEmpty()) {
@@ -39,7 +42,7 @@ public class ClientService {
     }
     return clientsDto;
   }
-  
+
   public Optional<ClientDto> save(ClientDto clientDto) {
     if (Optional.of(clientDto).isPresent()) {
       Client client = Helper.toClient(clientDto);
@@ -51,7 +54,7 @@ public class ClientService {
     }
     return Optional.empty();
   }
-  
+
   public Optional<ClientDto> findById(Long id) {
     if (!Optional.of(id).isEmpty()) {
       Optional<Client> client = this.clientRepository.findById(id);
@@ -60,5 +63,17 @@ public class ClientService {
     }
     return Optional.empty();
   }
-  
+
+  public boolean deleteById(Long id) {
+    if (id != null) {
+      Optional<Client> client = this.clientRepository.findById(id);
+      if (!client.isEmpty()) {
+        this.clientRepository.deleteById(id);
+        return true;
+      }
+    }
+    log.info("Client ID "+id+"does not exist in database so could not be deleted");
+    return false;
+  }
+
 }
--- taco-cloud-jpa/src/main/java/tacos/web/api/TacoClientController.java
+++ taco-cloud-jpa/src/main/java/tacos/web/api/TacoClientController.java
@@ -3,10 +3,13 @@ package tacos.web.api;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,15 +27,15 @@ import tacos.dto.TacoOrderDto;
 @Slf4j
 @RestController
 @RequestMapping(path = "/api/clients", produces = "application/json")
-@CrossOrigin(origins = {"http://tacocloud:8080", "http://tacocloud.com"})
+@CrossOrigin(origins = { "http://tacocloud:8080", "http://tacocloud.com" })
 public class TacoClientController {
-  
+
   private ClientService clientService;
 
   public TacoClientController(ClientService clientService) {
     this.clientService = clientService;
   }
-  
+
   @GetMapping(params = "all")
   public List<ClientDto> allClients() {
     List<ClientDto> clients = clientService.findAll();
@@ -48,19 +51,67 @@ public class TacoClientController {
     }
     return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
   }
-  
-  @PostMapping(consumes="application/json")
+
+  @PostMapping(consumes = "application/json")
   @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<ClientDto> postClient(@RequestBody ClientDto client) {
     List<ClientDto> clients = this.clientService.findByDeliveryName(client.getDeliveryName());
     if (!clients.isEmpty())
       return new ResponseEntity<>(clients.get(0), HttpStatus.CONFLICT);
-    
+
     Optional<ClientDto> optClient = this.clientService.save(client);
     if (optClient.isPresent()) {
       return new ResponseEntity<>(optClient.get(), HttpStatus.OK);
     }
     return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
   }
+
+  @PatchMapping(path = "/{clientId}", consumes = "application/json")
+  public ResponseEntity<ClientDto> patchClient(@PathVariable("clientId") Long clientId, @RequestBody ClientDto patch) {
+
+    Optional<ClientDto> clientDto = this.clientService.findById(clientId);
+    if (clientDto.isPresent()) {
+      if (patch.getDeliveryName() != null) {
+        clientDto.get().setDeliveryName(patch.getDeliveryName());
+      }
+      if (patch.getDeliveryStreet() != null) {
+        clientDto.get().setDeliveryStreet(patch.getDeliveryStreet());
+      }
+      if (patch.getDeliveryCity() != null) {
+        clientDto.get().setDeliveryCity(patch.getDeliveryCity());
+      }
+      if (patch.getDeliveryState() != null) {
+        clientDto.get().setDeliveryState(patch.getDeliveryState());
+      }
+      if (patch.getDeliveryZip() != null) {
+        clientDto.get().setDeliveryZip(patch.getDeliveryZip());
+      }
+      if (patch.getCcNumber() != null) {
+        clientDto.get().setCcNumber(patch.getCcNumber());
+      }
+      if (patch.getCcExpiration() != null) {
+        clientDto.get().setCcExpiration(patch.getCcExpiration());
+      }
+      if (patch.getCcCVV() != null) {
+        clientDto.get().setCcCVV(patch.getCcCVV());
+      }
+      Optional<ClientDto> savedDto = this.clientService.save(clientDto.get());
+      return new ResponseEntity<>(savedDto.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+  }
   
+  @DeleteMapping("/{clientId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public ResponseEntity<ClientDto> deleteOrder(@PathVariable("clientId") Long clientId) {
+    boolean isDeleted = this.clientService.deleteById(clientId);
+    if (isDeleted) {
+      Optional<ClientDto> optClient = this.clientService.findById(clientId);
+      if (optClient.isPresent()) {
+        return new ResponseEntity<>(optClient.get(), HttpStatus.OK);
+      }
+    }
+    return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+  }
+
 }
