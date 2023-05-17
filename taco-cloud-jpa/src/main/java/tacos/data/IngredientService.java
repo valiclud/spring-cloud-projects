99c9eeb0b16aa8cc73584aab22c25c8481d9f043
valiclud <valicek@atlas.cz>
2023-05-17 11:34:52 +0200

    Configuring Ingredient Rest Controller

--- taco-cloud-jpa/src/main/java/tacos/data/IngredientService.java
+++ taco-cloud-jpa/src/main/java/tacos/data/IngredientService.java
@@ -2,11 +2,14 @@ package tacos.data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tacos.dto.ClientDto;
 import tacos.dto.IngredientDto;
+import tacos.entity.Client;
 import tacos.entity.Ingredient;
 
 @Service
@@ -21,11 +24,34 @@ public class IngredientService {
 		return ingredientsToDto(ingreds);
 	}
 	
+  public Optional<IngredientDto> findById(Long id) {
+    if (!Optional.of(id).isEmpty()) {
+      Optional<Ingredient> ingredient = this.ingredientRepository.findById(id);
+      if (!ingredient.isEmpty())
+        return Optional.of(Helper.toIngredientDto(ingredient.get()));
+    }
+    return Optional.empty();
+  }
+
+	
 	public List<IngredientDto> findByCode(String code)  {
 		Iterable<Ingredient> ingreds = this.ingredientRepository.findByCode(code);
 		
 		return ingredientsToDto(ingreds);
 	}
+	
+  public Optional<IngredientDto> save(IngredientDto ingredientDto) {
+    if (Optional.of(ingredientDto).isPresent()) {
+      Ingredient ingredient = Helper.toIngredient(ingredientDto);
+      Ingredient ingredientSaved = this.ingredientRepository.save(ingredient);
+      if (Optional.of(ingredientSaved).isPresent()) {
+        IngredientDto ingredientDtoSaved = Helper.toIngredientDto(ingredientSaved);
+        return Optional.of(ingredientDtoSaved);
+      }
+    }
+    return Optional.empty();
+  }
+
 	
 	private List<IngredientDto> ingredientsToDto(Iterable<Ingredient> ingreds){
 		List<IngredientDto> listIngreds = new ArrayList<>();
---
+++ taco-cloud-jpa/src/main/java/tacos/web/api/IngredientController.java
@@ -0,0 +1,69 @@
+package tacos.web.api;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import tacos.data.IngredientService;
+import tacos.dto.ClientDto;
+import tacos.dto.IngredientDto;
+
+@RestController
+@RequestMapping(path="/api/ingredients", produces="application/json")
+@CrossOrigin(origins="http://localhost:8080")
+public class IngredientController {
+
+  private IngredientService service;
+
+  @Autowired
+  public IngredientController(IngredientService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public List<IngredientDto> allIngredients() {
+    return service.findAll();
+  }
+  
+/*
+  @GetMapping
+  public Map<String, List<IngredientDto>> allIngredients() {
+    return Collections.singletonMap("ingredients", service.findAll());
+  }
+*/
+  
+  @GetMapping("/{id}")
+  public ResponseEntity<IngredientDto> clientById(@PathVariable("id") Long id) {
+    Optional<IngredientDto> optIngredient = this.service.findById(id);
+    if (optIngredient.isPresent()) {
+      return new ResponseEntity<>(optIngredient.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+  }
+  
+  @PostMapping(consumes = "application/json")
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<IngredientDto> postIngredient(@RequestBody IngredientDto ingredientDto) {
+    Optional<IngredientDto> optIngredient = this.service.save(ingredientDto);
+    if (optIngredient.isPresent()) {
+      return new ResponseEntity<>(optIngredient.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+  }
+
+  
+}
