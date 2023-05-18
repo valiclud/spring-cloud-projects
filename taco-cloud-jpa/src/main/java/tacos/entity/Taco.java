03eb34bfbf676f186261e0e4ece5da247efa9202
valiclud <valicek@atlas.cz>
2023-05-18 08:20:56 +0200

    Adding RestResource

--- taco-cloud-jpa/src/main/java/tacos/entity/Taco.java
+++ taco-cloud-jpa/src/main/java/tacos/entity/Taco.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +17,7 @@ import lombok.Data;
 
 @Data
 @Entity
+@RestResource(rel="tacos", path="tacos")
 public class Taco {
  
   @Id
