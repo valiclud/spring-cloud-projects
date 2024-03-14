d2a4b4388548b6f79a7619dde64c4cd84cd351ff
valiclud <valicek@atlas.cz>
2024-03-14 11:11:44 +0100

    OA2 authoriyation playing.

---
+++ taco-cloud-oa2-server/src/test/java/tacos/authorization/TacoCloudOa2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudOa2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
